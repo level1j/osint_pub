53c56e9768c709604354648207c160ce6900682a
level1j <level1@jh77.org>
2020-06-10 12:16:21 +0900

    bug fix screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -95,10 +95,11 @@ def save_screenshot_firefox(url, useragent, mode=MODE_SMARTPHONE):
 def wevdriver_initialize_chrome(useragent, mode=MODE_SMARTPHONE):
     options = Options()
     options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
     options.add_argument('user-agent=' + useragent)
-    options.add_argument('--ignore-certificate-errors')
-    driver = webdriver.Chrome(options=options)
+    options.add_argument('--ignore-certificate-errors')    driver = webdriver.Chrome(options=options)
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
     if mode == MODE_SMARTPHONE:
         driver.set_window_size(SMARTPHONE_WIDTH, SMARTPHONE_HEIGHT)
     return driver
