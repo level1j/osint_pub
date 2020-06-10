b866a2aa680a74d4cb5c173169360917700b8aa0
level1j <level1@jh77.org>
2020-06-10 12:17:47 +0900

    bug fix screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -96,10 +96,11 @@ def wevdriver_initialize_chrome(useragent, mode=MODE_SMARTPHONE):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('user-agent=' + useragent)
-    options.add_argument('--ignore-certificate-errors')    driver = webdriver.Chrome(options=options)
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(options=options)
     if mode == MODE_SMARTPHONE:
         driver.set_window_size(SMARTPHONE_WIDTH, SMARTPHONE_HEIGHT)
     return driver
